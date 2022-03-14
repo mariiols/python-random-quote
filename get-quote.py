1336770def836de9ff26ab8f709b820e1a102c1a
Mario Martin <Mario.Martin@dentsu.com>
2022-03-14 16:22:08 +0100

    Funcion Primary

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,17 @@
 def primary():
     f = open("quotes.txt")
     quotes = f.readlines()
+    for j in range (0,len(quotes)):
+        quotes[j]=quotes[j].replace("\n","")
     f.close()
-    
-    print(quotes[13])
+    print(quotes[14:18])
+        
+    mi_path = "fichero.txt"
+    g = open(mi_path, 'a+')
+    for i in quotes:
+        g.write(i)
+        g.write("\n")
+    g.close()
 
 if __name__== "__main__":
     primary()
